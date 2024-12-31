78cdbc22180e002575efa5aa38ba2fe3ea35968d
5kydriven <tagpuno619@gmail.com>
2024-12-31 18:58:36 +0800

    feat: optimize role and alumni fetching in middleware for improved performance

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,17 +2,17 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 	if (import.meta.client) return;
 
 	const user = await getCurrentUser();
-
+	console.log('User: ', user);
 	console.log('To Path:', to.path);
-
-	if (user && to.path == '/auth') {
-		return await navigateTo('/');
-	}
 
 	const paths = ['/admin', '/registrar', '/employer', '/alumni'];
 	if (!user && paths.includes(to.path)) {
 		console.log('Redirecting to /auth');
 		return await navigateTo('/auth');
+	}
+
+	if (user && to.path == '/auth') {
+		return await navigateTo('/');
 	}
 
 	// if (user && paths.includes(from.path)) {
