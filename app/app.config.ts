157f0c18598d6ec67ac64a420c4abeb148f40fce
5kydriven <tagpuno619@gmail.com>
2025-01-15 21:40:05 +0800

    feat: enhance alumni and employer navbars with new styles, add logo, and improve layout

--- app/app.config.ts
+++ app/app.config.ts
@@ -17,6 +17,13 @@ export default defineAppConfig({
 			inactive:
 				'text-white dark:text-gray-400 hover:text-black dark:hover:text-white hover:before:bg-yellow-300 dark:hover:before:bg-gray-800/50',
 		},
+		horizontalNavigation: {
+			before: 'before:rounded-none hover:before:bg-transparent',
+			after:
+				'after:absolute after:bottom-0 after:inset-x-2.5 after:block after:h-[2px] after:mt-2',
+			active: 'text-yellow-500 border-b border-yellow-500 rounded-none',
+			inactive: 'text-white hover:text-yellow hover:bg-transparent',
+		},
 		avatar: {
 			background: 'bg-gray-200',
 		},
