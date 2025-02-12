095065254f05f57f8870f76b666acb6de416f35e
ccwebgroup <ccwebgroup@gmail.com>
2025-02-12 23:33:30 +0800

    fix: firebase-admin config via .env variables

--- server/plugins/firebase.ts
+++ server/plugins/firebase.ts
@@ -1,6 +1,13 @@
-import initFirebase from '../utils/initFirebase';
+import initFirebase from "../utils/initFirebase";
 
 export default defineNitroPlugin((nuxtapp) => {
-	const firebaseConfig = ;
-	initFirebase(firebaseConfig);
+  const firebaseConfig = JSON.parse(
+    process.env.GOOGLE_APPLICATION_CREDENTIALS as string
+  );
+
+  if (firebaseConfig.private_key) {
+    firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
+  }
+  
+  initFirebase(firebaseConfig);
 });
