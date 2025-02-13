232ad41bd3e66999d3f47b0d867863e20011ef33
5kydriven <tagpuno619@gmail.com>
2025-02-13 09:20:21 +0800

    refactor: update Firebase initialization to use runtime config; clean up test credentials formatting

--- server/plugins/firebase.ts
+++ server/plugins/firebase.ts
@@ -1,13 +1,18 @@
-import initFirebase from "../utils/initFirebase";
+// import initFirebase from '../utils/initFirebase';
 
 export default defineNitroPlugin((nuxtapp) => {
-  const firebaseConfig = JSON.parse(
-    process.env.GOOGLE_APPLICATION_CREDENTIALS as string
-  );
+	const config = useRuntimeConfig();
 
-  if (firebaseConfig.private_key) {
-    firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
-  }
-  
-  initFirebase(firebaseConfig);
+	const firebaseConfig = JSON.parse(
+		config.googleApplicationCredentials as string,
+	);
+
+	if (firebaseConfig.private_key) {
+		firebaseConfig.private_key = firebaseConfig.private_key.replace(
+			/\\n/g,
+			'\n',
+		);
+	}
+
+	initFirebase(firebaseConfig);
 });
