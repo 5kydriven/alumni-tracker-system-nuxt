9d47bc6cb56e27b7d73bfe50ce76b8950ca1b667
5kydriven <tagpuno619@gmail.com>
2025-02-23 21:05:52 +0800

    feat: integrate slide-over component for message conversation header

--- app/pages/registrar/messages.vue
+++ app/pages/registrar/messages.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+	import { RegistrarSlideOver } from '#components';
 	definePageMeta({
 		middleware: ['personel'],
 		layout: 'registrar',
 	});
 
 	const router = useRouter();
+	const slideOver = useSlideover();
 
 	function handleClick(uid: string) {
 		router.push(`/registrar/messages/${uid}`);
@@ -16,12 +18,12 @@
 		class="flex h-full xl:border xl:border-b-none xl:rounded-t dark:border-gray-800">
 		<div
 			class="border-r-none w-full border-gray-300 md:border-r bg-white dark:border-gray-800 shadow-lg lg:max-w-md flex flex-col">
-			<div
-				class="h-16 px-4 flex items-center border-b dark:border-gray-800 border-gray-200 bg-gray-300 shrink-0">
-				<div class="flex gap-2 items-center">
-					<label class="text-lg font-bold">Messages</label>
-				</div>
-			</div>
+			<ConversationHeader
+				class="py-4"
+				@click="
+					slideOver.open(RegistrarSlideOver, { onClose: slideOver.close })
+				"
+				isShowButton />
 
 			<ConversationContainer @itemClicked="handleClick" />
 		</div>
